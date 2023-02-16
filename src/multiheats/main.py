e2116b7b08135155ba67858954cbf832e0c0c3f4
Cyril Mergny <cyril.mergny@universite-paris-saclay.fr>
2023-02-16 11:45:06 +0100

    Working on the README

--- src/multiheats/main.py
+++ src/multiheats/main.py
@@ -31,28 +31,6 @@ import visualise as vis
 ### MAIN
 
 
-def animate_function(spaces, temps, interf=0, step=1, frames=None, save=False):
-    """Plot an animation of the temperature with time"""
-    frames = temps.shape[0] // step if frames is None else frames
-    fig, ax = plt.subplots()
-    (line,) = ax.plot([])
-    ax.axvline(x=interf, alpha=0.3, linestyle="-", color="black")
-
-    def animate(it):
-        line.set_data((spaces, temps[it * step]))
-        # ax.set_title(f"{it}")
-        return line
-
-    ax.set_ylabel("Temperature (K)")
-    ax.set_xlabel("Depth (m)")
-    ax.set_xlim(spaces.min(), spaces.max())
-    ax.set_ylim(temps.min(), temps.max())
-    ax.set_xscale("symlog")
-    anim = FuncAnimation(fig, animate, frames=frames, interval=1, repeat=False)
-    plt.title("Temperature Evolution")
-    return anim
-
-
 if __name__ == "__main__":
 
     print("Creating surface profile...")
@@ -65,7 +43,7 @@ if __name__ == "__main__":
     temp_eq = surf.get_eq_temp(prof.lat, prof.long, prof.eps)
     prof.temp = np.full(prof.nx, temp_eq)
 
-    times = surf.times[:]
+    times = surf.times[:1000]
     nt = times.shape[0]
     dt = np.diff(times)[0]
     temps = np.zeros((nt, prof.nx))
@@ -79,27 +57,9 @@ if __name__ == "__main__":
         temps[it] = prof.temp
 
     print("Visualisation")
-    # Visualisation
-    it = 300
-    # vis.plot_temp(prof.spaces, temps, it)
+    it = 490
+    vis.use_latex()
+    vis.plot_temp(prof.spaces, temps, it, interf=prof.interf)
     # vis.plot_multi_temp(prof.spaces, temps, n_curves=10)
-
-    anim = animate_function(prof.spaces, temps, interf=prof.interf, step=5, frames=350)
-    plt.show()
-
-    tex_fonts = {
-        # Use LaTeX to write all text
-        "text.usetex": True,
-        "font.family": "serif",
-        # Use 10pt font in plots, to match 10pt font in document
-        "axes.labelsize": 13,
-        "font.size": 11,
-        # Make the legend/label fonts a little smaller
-        "legend.fontsize": 11,
-        "xtick.labelsize": 11,
-        "ytick.labelsize": 11,
-    }
-    plt.rcParams.update(tex_fonts)
-
-    progress_callback = lambda i, n: print(f"Saving frame {i} of {n}")
-    anim.save("animation.gif", progress_callback=progress_callback)
+    # anim = animate_function(prof.spaces, temps, interf=prof.interf, step=5, frames=350)
+    # plt.show()
--- src/multiheats/visualise.py
+++ src/multiheats/visualise.py
@@ -2,6 +2,22 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 
+def use_latex():
+    tex_fonts = {
+        # Use LaTeX to write all text
+        "text.usetex": True,
+        "font.family": "serif",
+        # Use 10pt font in plots, to match 10pt font in document
+        "axes.labelsize": 13,
+        "font.size": 11,
+        # Make the legend/label fonts a little smaller
+        "legend.fontsize": 11,
+        "xtick.labelsize": 11,
+        "ytick.labelsize": 11,
+    }
+    plt.rcParams.update(tex_fonts)
+
+
 def plot_multi_temp(spaces, temps, n_curves=10):
     """Plot multiple temperature profiles chosen evenly among all times."""
     fig, ax = plt.subplots()
@@ -15,22 +31,29 @@ def plot_multi_temp(spaces, temps, n_curves=10):
     plt.show()
 
 
-def plot_temp(spaces, temps, it):
+def plot_temp(spaces, temps, it, interf=0):
     """Plot one temperature profile at one time."""
+    mean_temp = temps[it].mean()
     fig, ax = plt.subplots()
-    ax.plot(spaces, temps[it], label=f"{it}")
-    ax.set_xlabel("x (m)")
+    ax.axvline(x=interf, alpha=0.2, linestyle="-", color="black", label="Interface")
+    ax.plot(spaces, temps[it])
+    ax.set_xlabel("Depth (m)")
     ax.set_ylabel("Temperature (K)")
-    ax.set_ylim(temps.min(), temps.max())
-    ax.set_xscale("log")
+    ax.set_ylim(0.9 * mean_temp, 1.1 * mean_temp)
+    ax.set_xlim(0, spaces.max())
+    ax.set_xscale("symlog")
+    plt.title("Bilayer temperature profile snapshot")
     plt.legend()
-    plt.show()
+    # plt.show()
+    plt.savefig("../../examples/temp_bilayer.png")
 
 
-def animate_function(spaces, temps, step=1, save=False):
+def animate_function(spaces, temps, interf=0, step=1, frames=None, save=False):
     """Plot an animation of the temperature with time"""
-    fig, ax = plt.subplots(figsize=(5, 3.5))
+    frames = temps.shape[0] // step if frames is None else frames
+    fig, ax = plt.subplots()
     (line,) = ax.plot([])
+    ax.axvline(x=interf, alpha=0.3, linestyle="-", color="black")
 
     def animate(it):
         line.set_data((spaces, temps[it * step]))
@@ -42,6 +65,10 @@ def animate_function(spaces, temps, step=1, save=False):
     ax.set_xlim(spaces.min(), spaces.max())
     ax.set_ylim(temps.min(), temps.max())
     ax.set_xscale("symlog")
-    anim = FuncAnimation(fig, animate, frames=temps.shape[0], interval=1, repeat=True)
+    anim = FuncAnimation(fig, animate, frames=frames, interval=1, repeat=False)
     plt.title("Temperature Evolution")
+
+    if save:
+        progress_callback = lambda i, n: print(f"Saving frame {i} of {n}")
+        anim.save("temp_evo.gif", progress_callback=progress_callback)
     return anim
