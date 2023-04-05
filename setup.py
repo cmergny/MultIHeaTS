a26354b9d9f28562d55cd88c60f9cfcddd050483
Cyril Mergny <cyril.mergny@universite-paris-saclay.fr>
2023-04-05 14:57:39 +0100

    Changed gitlab cli to use pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="multiheats",
-    version="0.2",
+    version="0.3",
     packages=find_packages("src"),
     package_dir={"": "src"},
     description="MultIHeaTS is a Multi-layered Implicit Heat Transfer Solver. It uses finite differences to  simulates and predicts the surface temperature in 1D multi-layered planetary surfaces exposed to solar radiation.",
