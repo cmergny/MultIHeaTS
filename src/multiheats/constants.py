c38968e0d43b090f52485b2a0842af547d2e8cc8
Cyril Mergny <cyril.mergny@universite-paris-saclay.fr>
2023-05-03 17:24:10 +0200

    Remove unsused constants

--- src/multiheats/constants.py
+++ src/multiheats/constants.py
@@ -18,21 +18,3 @@ RGAS = 8.31446261815324  # Gas constant J.K-1.mol-1
 
 # WATER
 MOL_WATER = 18e-3  # Molar mass of water kg.mol-1
-
-# METAMORPHISM
-P_WATER_TRIPLE = 611.73  # (Pa) for triple pt of water
-T_WATER_TRIPLE = 273.16  # (K) for triple pt of water
-D_VAPOR_AIR = 21.0e-6  # (m2/s) diff of water vapor in air Collbeck 93
-D_VAPOR_SNOW = 85.0e-6  # (m2/s) diff of water vapor in snow (ok for ice?)
-K_ICE = 2.2  # Conductivity of ice (W.m-1.K-1)
-K_AIR = K_ICE / 100  # Conductivity of air (W.m-1.K-1)
-RHO_ICE = 917  # density pure ice SI (valid for T=0 C only!!)
-LSUB_ICE = 2.838e6  # (J/kg) latent heat sublimation of ice
-R_ICE = 461.9  # (?) gas constant for ice
-GAMMA_S = 0.06  # (J/m2) surfance tension /!\ may vary in litt
-## SNOWPACK PARAMS
-FGG = 3  # geom factor eq9 snowpackII
-FGB = 0.35  # geom factor eq14 snowpackII
-NSGZ = 0.15e-3  # (m) new snow grain size
-PSAT_C2 = 21.88  # empirical coeff found in Murrar1966
-PSAT_C3 = 7.66  # empirical coeff found in Murrar1966
